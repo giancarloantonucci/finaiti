9652b397160adb685385eecc1a6ec1cb91ec85e9
Giancarlo A. Antonucci <giancarlo.antonucci@icloud.com>
2025-03-14 19:28:24 +0000

    Attualizza finaiti

--- pulizziata.py
+++ pulizziata.py
@@ -1,49 +1,109 @@
-from geopandas import read_file
-from pandas import concat, read_csv
+# %%
+from geopandas import read_file, GeoDataFrame
+from pandas import concat, read_csv, NA
 
-# Carrica dati ISTAT
-riggiuni_italia = read_file("./Limiti01012023/Reg01012023/Reg01012023_WGS84.shp")
-pruvinci_italia = read_file("./Limiti01012023/ProvCM01012023/ProvCM01012023_WGS84.shp")
-cumuna_italia = read_file("./Limiti01012023/Com01012023/Com01012023_WGS84.shp")
+# %% Càrrica i dati Istat
+riggiuni_italia = read_file("./Limiti01012025/Reg01012025/Reg01012025_WGS84.shp")
+pruvinci_italia = read_file("./Limiti01012025/ProvCM01012025/ProvCM01012025_WGS84.shp")
+cumuna_italia = read_file("./Limiti01012025/Com01012025/Com01012025_WGS84.shp")
 
-# Sarba i cunfini dâ riggiuni_sicilia, zuè dâ Sicilia
-riggiuni_sicilia = riggiuni_italia[['DEN_REG', 'geometry']][riggiuni_italia['DEN_REG'] == 'Sicilia'].rename(columns={'DEN_REG': 'ITA'}).reset_index(drop=True)
+# %% Sarba i cunfini dî riggiuni
+riggiuni_sicilia = (
+    riggiuni_italia[['DEN_REG', 'geometry']][riggiuni_italia['DEN_REG'] == 'Sicilia']
+    .rename(columns={'DEN_REG': 'ITA'})
+    .reset_index(drop=True)
+)
 riggiuni_sicilia['SCN'] = 'Sicilia'
-riggiuni_sicilia = riggiuni_sicilia[['SCN', 'ITA', 'geometry']]
-riggiuni_sicilia.to_crs(epsg=4326).to_file('./riggiuni/riggiuni.shp', encoding='utf-8')
 
-# Sarba i cunfini dî pruvinci_sicilia
-pruvinci_sicilia = pruvinci_italia[['DEN_UTS', 'COD_UTS', 'geometry']][pruvinci_italia['COD_REG'] == 19].rename(columns={'DEN_UTS': 'ITA', 'COD_UTS': 'PROVINCE'}).reset_index(drop=True)
-pruvinci_sicilia['SCN'] = ''
-pruvinci_sicilia.loc[pruvinci_sicilia['ITA'] == 'Catania', 'SCN'] = 'Catania'
-pruvinci_sicilia.loc[pruvinci_sicilia['ITA'] == 'Messina', 'SCN'] = 'Missina'
-pruvinci_sicilia.loc[pruvinci_sicilia['ITA'] == 'Palermo', 'SCN'] = 'Palermu'
-pruvinci_sicilia.loc[pruvinci_sicilia['ITA'] == 'Agrigento', 'SCN'] = 'Girgenti'
-pruvinci_sicilia.loc[pruvinci_sicilia['ITA'] == 'Caltanissetta', 'SCN'] = 'Nissa'
-pruvinci_sicilia.loc[pruvinci_sicilia['ITA'] == 'Enna', 'SCN'] = 'Castruggiuvanni'
-pruvinci_sicilia.loc[pruvinci_sicilia['ITA'] == 'Ragusa', 'SCN'] = 'Ragusa'
-pruvinci_sicilia.loc[pruvinci_sicilia['ITA'] == 'Siracusa', 'SCN'] = 'Saragusa'
-pruvinci_sicilia.loc[pruvinci_sicilia['ITA'] == 'Trapani', 'SCN'] = 'Tràpani'
-pruvinci_sicilia = pruvinci_sicilia[['SCN', 'ITA', 'PROVINCE', 'geometry']]
-pruvinci_sicilia.to_crs(epsg=4326).to_file('./pruvinci/pruvinci.shp', encoding='utf-8')
+riggiuni_calabbria = (
+    riggiuni_italia[['DEN_REG', 'geometry']][riggiuni_italia['DEN_REG'] == 'Calabria']
+    .rename(columns={'DEN_REG': 'ITA'})
+    .reset_index(drop=True)
+)
+riggiuni_calabbria['SCN'] = 'Calabbria'
 
-# Sarba i cunfini dî cumuna_sicilia
-cumuna_sicilia = cumuna_italia[['COMUNE', 'COD_UTS', 'geometry']][cumuna_italia['COD_REG'] == 19].rename(columns={'COMUNE': 'ITA', 'COD_UTS': 'PROVINCE'}).reset_index(drop=True)
-cumuna_sicilia['SCN'] = ''
-cumuna_sicilia['LOCAL'] = ''
-cumuna_sicilia['IPA'] = ''
-cumuna_sicilia['DEMONYM'] = ''
-cumuna_sicilia['FROM_CS'] = 0
-cumuna_sicilia = cumuna_sicilia[['SCN', 'ITA', 'PROVINCE', 'LOCAL', 'IPA', 'DEMONYM', 'FROM_CS', 'geometry']]
-cumuna_sicilia = cumuna_sicilia.replace({'Ã¬': 'ì', 'Ã¹': 'ù', 'Ã²': 'ò', 'Ã': 'à', "\xa0": ''}, regex=True)
+riggiuni_junciuti = GeoDataFrame(concat([riggiuni_sicilia, riggiuni_calabbria], ignore_index=True))
+riggiuni_junciuti = riggiuni_junciuti[['SCN', 'ITA', 'geometry']]
+riggiuni_junciuti.to_crs(epsg=4326).to_file('./riggiuni/riggiuni.shp', encoding='utf-8')
 
-# Metti macari i noma dî cumuna_sicilia 'n sicilianu
+# %% Sarba i cunfini dî pruvinci
+pruvinci_sicilia = (
+    pruvinci_italia[['DEN_UTS', 'COD_UTS', 'geometry']][pruvinci_italia['COD_REG'] == 19]
+    .rename(columns={'DEN_UTS': 'ITA', 'COD_UTS': 'PROVINCE'})
+    .reset_index(drop=True)
+)
+noma_pruvinci_sicilia = {
+    'Catania': 'Catania',
+    'Messina': 'Missina',
+    'Palermo': 'Palermu',
+    'Agrigento': 'Girgenti',
+    'Caltanissetta': 'Nissa',
+    'Enna': 'Castruggiuvanni',
+    'Ragusa': 'Ragusa',
+    'Siracusa': 'Saragusa',
+    'Trapani': 'Tràpani'
+}
+pruvinci_sicilia['SCN'] = pruvinci_sicilia['ITA'].map(noma_pruvinci_sicilia)
+
+pruvinci_calabbria = (
+    pruvinci_italia[['DEN_UTS', 'COD_UTS', 'geometry']]
+    [(pruvinci_italia['COD_REG'] == 18) & (pruvinci_italia['DEN_UTS'] == 'Reggio di Calabria')]
+    .rename(columns={'DEN_UTS': 'ITA', 'COD_UTS': 'PROVINCE'})
+    .reset_index(drop=True)
+)
+noma_pruvinci_calabbria = {
+    'Reggio di Calabria': 'Riggiu'
+}
+pruvinci_calabbria['SCN'] = pruvinci_calabbria['ITA'].map(noma_pruvinci_calabbria)
+
+pruvinci_junciuti = GeoDataFrame(concat([pruvinci_sicilia, pruvinci_calabbria], ignore_index=True))
+pruvinci_junciuti = pruvinci_junciuti[['SCN', 'ITA', 'PROVINCE', 'geometry']]
+pruvinci_junciuti.to_crs(epsg=4326).to_file('./pruvinci/pruvinci.shp', encoding='utf-8')
+
+# %% Sarba i cunfini dî cumuna
+cumuna_sicilia = (
+    cumuna_italia[['COMUNE', 'COD_UTS', 'geometry']][cumuna_italia['COD_REG'] == 19]
+    .rename(columns={'COMUNE': 'ITA', 'COD_UTS': 'PROVINCE'})
+    .reset_index(drop=True)
+)
+
+cumuna_siculofuni = [
+    'Scilla', 'Roghudi', 'Bova', 'Bova Marina', 'Condofuri', 'Roccaforte del Greco',
+    'Santo Stefano in Aspromonte', 'San Roberto', 'Fiumara', 'Campo Calabro',
+    'Villa San Giovanni', 'Reggio di Calabria', 'Calanna', 'Laganadi',
+    'Sant\'Alessio in Aspromonte', 'Cardeto', 'Bagaladi', 'San Lorenzo',
+    'Motta San Giovanni', 'Montebello Jonico', 'Melito di Porto Salvo'
+]
+cumuna_calabbria = (
+    cumuna_italia[['COMUNE', 'COD_UTS', 'geometry']]
+    [
+        (cumuna_italia['COD_REG'] == 18) &
+        (cumuna_italia['COD_PROV'] == 80) &
+        (cumuna_italia['COMUNE'].isin(cumuna_siculofuni))
+    ]
+    .rename(columns={'COMUNE': 'ITA', 'COD_UTS': 'PROVINCE'})
+    .reset_index(drop=True)
+)
+
+cumuna_junciuti = GeoDataFrame(concat([cumuna_sicilia, cumuna_calabbria], ignore_index=True))
+
+cumuna_junciuti['SCN'] = ''
+cumuna_junciuti['LOCAL'] = ''
+cumuna_junciuti['DEMONYM'] = ''
+cumuna_junciuti['FROM'] = ''
+cumuna_junciuti = cumuna_junciuti[['SCN', 'ITA', 'PROVINCE', 'LOCAL', 'DEMONYM', 'FROM', 'geometry']]
+
+cumuna_junciuti = cumuna_junciuti.replace({'Ã¬': 'ì', 'Ã¹': 'ù', 'Ã²': 'ò', 'Ã': 'à', "\xa0": ''}, regex=True)
+
+# %% Metti macari i noma dî cumuna 'n sicilianu
 tupònimi = read_csv('./tuponimi.csv')
 for index, row in tupònimi.iterrows():
-    cumuna_sicilia.loc[cumuna_sicilia['ITA'] == row['ITA'], ['SCN', 'LOCAL', 'IPA', 'DEMONYM', 'FROM_CS']] = [row['SCN'], row['LOCAL'], row['IPA'], row['DEMONYM'], row['FROM_CS']]
+    cumuna_junciuti.loc[cumuna_junciuti['ITA'] == row['ITA'], ['SCN', 'LOCAL', 'DEMONYM', 'FROM']] = [
+        row['SCN'], row['LOCAL'], row['DEMONYM'], row['FROM']
+    ]
 
-cumuna_sicilia.to_crs(epsg=4326).to_file('./cumuna/cumuna.shp', encoding='utf-8')
+cumuna_junciuti.to_crs(epsg=4326).to_file('./cumuna/cumuna.shp', encoding='utf-8')
 
-# Sarba tutti tri nzèmmula
-junciuti = concat([riggiuni_sicilia, pruvinci_sicilia, cumuna_sicilia]).reset_index(drop=True)
+# %% Sarba tutti tri nzèmmula
+junciuti = concat([riggiuni_junciuti, pruvinci_junciuti, cumuna_junciuti]).reset_index(drop=True)
 junciuti.to_crs(epsg=4326).to_file('./junciuti/junciuti.shp', encoding='utf-8')
